29b0ac6afddcfea09505df4e3d6d0a068f9b84c7
BcomedianC <43193347+BcomedianC@users.noreply.github.com>
2020-09-27 23:22:52 -0400

    Add files via upload

---
+++ NLP_gcloud/NLPtest.py
@@ -0,0 +1,76 @@
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+from google.cloud import language_v1
+from google.cloud.language_v1 import enums
+import sys
+
+'''
+# Instantiates a client
+client = language.LanguageServiceClient()
+content = sys.argv[1]
+# The text to analyze
+text = content
+document = types.Document(
+    content=text,
+    type=enums.Document.Type.PLAIN_TEXT)
+# Detects the sentiment of the text
+sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+print('Text: {}'.format(text))
+print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+'''
+
+def sentiment(text_content):
+    """
+    Analyzing Sentiment in a String
+
+    Args:
+      text_content The text content to analyze
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    # text_content = 'I am so happy and joyful.'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = enums.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "en"
+    document = {"content": text_content, "type": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = enums.EncodingType.UTF8
+
+    response = client.analyze_sentiment(document, encoding_type=encoding_type)
+    # Get overall sentiment of the input document
+    print(u"Document sentiment score: {}".format(response.document_sentiment.score))
+    print(
+        u"Document sentiment magnitude: {}".format(
+            response.document_sentiment.magnitude
+        )
+    )
+    # Get sentiment for all sentences in the document
+    for sentence in response.sentences:
+        print(u"Sentence text: {}".format(sentence.text.content))
+        print(u"Sentence sentiment score: {}".format(sentence.sentiment.score))
+        print(u"Sentence sentiment magnitude: {}".format(sentence.sentiment.magnitude))
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print(u"Language of the text: {}".format(response.language))
+
+def main():
+	file1 = open("file.txt","r+")
+	text = file1.readline()
+	sentiment(text)
+	file1.close()
+
+
+if __name__ == '__main__':
+	main()
